bc65e217725eb2448c2b9770324b6c0f0986b308
Haibao Tang <tanghaibao@gmail.com>
2013-07-30 11:13:27 -0400

    update build_wgs.sh to use svn

--- build_wgs.sh
+++ build_wgs.sh
@@ -6,19 +6,17 @@ CXX=/usr/local/packages/gcc-4.4.3/bin/g++
 
 # Follow the instructions
 # <http://sourceforge.net/apps/mediawiki/wgs-assembler/index.php?title=Check_out_and_Compile>
-rm -rf wgs
-mkdir wgs
+#rm -rf wgs
+mkdir -p wgs
 cd wgs
 
-cvs -d:pserver:anonymous@wgs-assembler.cvs.sf.net:/cvsroot/wgs-assembler login
-cvs -d:pserver:anonymous@wgs-assembler.cvs.sf.net:/cvsroot/wgs-assembler co -P src
+svn co svn://svn.code.sf.net/p/wgs-assembler/svn/trunk/src src
+svn co svn://svn.code.sf.net/p/kmer/code/trunk kmer
 
-svn co https://kmer.svn.sourceforge.net/svnroot/kmer/trunk kmer
 cd kmer
-sh configure.sh
-make -j 16
-make install
+gmake -j 16
+gmake install
 cd ..
 
 cd src
-make -j 16
+gmake -j 16
